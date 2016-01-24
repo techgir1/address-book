d62d5945c22750a02eee81379c0ab0efc34a30e1
Beth Fostergit config --global user.namegit config --global user.name Beth <beth@techgir1.com>
2016-01-24 14:21:05 -0800

    fixed freezing

--- spec/specs.js
+++ spec/specs.js
@@ -11,3 +11,17 @@ it("adds the fullName method to all contacts", function() {
 	var testContact = new Contact("Jeff","Teseniar");
 	expect(testContact.fullName()).to.equal("Jeff Teseniar");
 });
+
+describe('Address', function() {
+  it("creates a new address with the given specifications", function() {
+    var testAddress = new Address("123 Main St", "Test City","Test State");
+    expect(testAddress.street).to.equal("123 Main St");
+    expect(testAddress.city).to.equal("Test City");
+    expect(testAddress.state).to.equal("Test State");
+  });
+});
+
+it("adds the fullAddress method to all addresses", function() {
+    var testAddress = new Address("123 Main St", "Test City","Test State");
+    expect(testAddress.fullAddress()).to.equal("123 Main St, Test City, Test State");
+  });
